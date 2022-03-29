3051ed031cae974b00292c33cf7ed473b08605cc
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-29 15:17:35 -0500

    Finish testing calculateScore method

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) {
         Score score = new Score();
-        System.out.println(score.calculateScore("dschlenk192", 23));
+        int myScore = score.calculateScore("dschlenk192", 23);
+        System.out.println("New score is " + myScore);
     }
 }
