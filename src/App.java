9f7daf166ff56f805bb3cc210639c2726fee0ae8
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-31 09:43:33 -0500

    Refactor first method

--- src/App.java
+++ src/App.java
@@ -2,8 +2,6 @@ public class App {
     public static void main(String[] args) {
         Measurements measure = new Measurements();
 
-        System.out.println(measure.calcFeetAndInchesToCentimeters(2, 8));
-
-        System.out.println(measure.calcFeetAndInchesToCentimeters(38));
+        System.out.println(measure.calcFeetAndInchesToCentimeters(6, 13));
     }
 }
--- src/Measurements.java
+++ src/Measurements.java
@@ -1,33 +1,47 @@
 public class Measurements {
-    public int calcFeetAndInchesToCentimeters(int feet, int inches) {
-        if ((feet >= 0) && (inches >= 0) && (inches <= 12)) {
-            // calculate how many centimeters comprise feet (30.48 cent in 1 ft)
-            double centInFt = Math.round(feet * 30.48);
+    // public int calcFeetAndInchesToCentimeters(int feet, int inches) {
+    //     if ((feet >= 0) && (inches >= 0) && (inches <= 12)) {
+    //         // calculate how many centimeters comprise feet (30.48 cent in 1 ft)
+    //         double centInFt = Math.round(feet * 30.48);
 
-            // calculate how many centimeters comprise inches (2.54 cent in 1 in)
-            double centInInch = Math.round(inches * 2.54);
+    //         // calculate how many centimeters comprise inches (2.54 cent in 1 in)
+    //         double centInInch = Math.round(inches * 2.54);
 
-            // convert back to int
-            int ftCentimeters = (int)centInFt;
-            int inchCentimeters = (int)centInInch;
+    //         // convert back to int
+    //         int ftCentimeters = (int)centInFt;
+    //         int inchCentimeters = (int)centInInch;
             
-            // return total centimeters
-            return ftCentimeters + inchCentimeters;
+    //         // return total centimeters
+    //         return ftCentimeters + inchCentimeters;
+    //     }
+
+    //     return -1;
+    // }
+
+    // // Method overloading
+    // public int calcFeetAndInchesToCentimeters(int inches) {
+    //     if (inches < 0) {
+    //         return -1;
+    //     }
+
+    //     // calculate how many feet are in inches
+    //     int feetInInches = Math.round(inches / 12);
+
+    //     // call first method
+    //     return calcFeetAndInchesToCentimeters(feetInInches, inches);
+    // }
+
+    public double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        // Validate
+        if ((feet < 0) || ((inches < 0) || (inches > 12))) {
+            System.out.println("Invalid");
+            return -1;
+        } else {
+            // With valid parameters...calculate centimeters for both feet and inches
+            double centimeters = (feet * 12) * 2.54;
+            centimeters += inches * 2.54;
+            System.out.println(feet + " feet " + inches + " inches = " + centimeters + " cm.");
+            return centimeters;
         }
-
-        return -1;
-    }
-
-    // Method overloading
-    public int calcFeetAndInchesToCentimeters(int inches) {
-        if (inches >= 0) {
-            // calculate how many feet are in inches
-            int feetInInches = Math.round(inches / 12);
-
-            // call first method
-            calcFeetAndInchesToCentimeters(feetInInches, inches);
-        }
-
-        return -1;
     }
 }
