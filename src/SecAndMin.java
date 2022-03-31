a6f4cac0cc5a3cf70fa298c1f0d1602d895919db
dayna-schlenk <dayna.schlenker@gmail.com>
2022-03-31 10:16:42 -0500

    Set up overloaded methods and write comments

--- src/SecAndMin.java
+++ src/SecAndMin.java
@@ -1,5 +1,30 @@
+/* Challenge tips:
+-> Use int & long for your number data types
+-> 1 minute = 60 seconds
+-> 1 hour = 60 minutes (or 3600 seconds)
+*/
+
 public class SecAndMin {
     public String getDurationString(int minutes, int seconds) {
-        // code
+        // Validate
+        if ((minutes >= 0) || ((seconds >= 0) && (seconds <= 59))) {
+            // Calculate how many hours, minutes, & seconds equal the minutes & seconds passed to this method
+
+            // Return that value as a String, formatted like so: XXh YYm ZZs (num hours, num minutes, & num seconds)
+        } else {
+            return "Invalid value";
+        }
+    }
+
+    // Method overloading
+    public String getDurationString(int seconds) {
+        // Validate
+        if (seconds >= 0) {
+            // Calculate how many minutes are in the seconds value
+
+            // Call the other overloaded method, passing the correct minutes & seconds calculated so that it can work properly
+        } else {
+            return "Invalid value";
+        }
     }
 }
