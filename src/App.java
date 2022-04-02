b3e4a85a3bf508913662ed30c0e7ba05bc77cce1
dayna-schlenk <dayna.schlenker@gmail.com>
2022-04-02 12:10:20 -0500

    Test logic in main to break down total seconds

--- src/App.java
+++ src/App.java
@@ -1,5 +1,40 @@
 public class App {
     public static void main(String[] args) {
-        // code
+        // SecAndMin myTry = new SecAndMin();
+        // System.out.println(myTry.getDurationString(170, 45));
+
+        // Testing...
+        int minutes = 70;
+        int seconds = 34;
+        int totalSeconds = (minutes * 60) + seconds;
+        System.out.println("Total seconds: " + totalSeconds);
+
+        int newHours;
+        int newMinutes;
+        int remainingSeconds;
+
+        if (totalSeconds >= 3600) {
+            newHours = totalSeconds / 3600;
+            System.out.println("New hours: " + newHours);
+            remainingSeconds = totalSeconds % 3600;
+
+            if (remainingSeconds >= 60) {
+                newMinutes = remainingSeconds / 60;
+                System.out.println("New minutes: " + newMinutes);
+                remainingSeconds %= 60;   
+            }
+
+            System.out.println("Remaining seconds: " + remainingSeconds);
+        }
+        
+        // if (totalSeconds >= 60) {
+        //     System.out.println("Not enough seconds to make an hour.");
+        //     newMinutes = totalSeconds / 60;
+        //     System.out.println("New minutes: " + newMinutes);
+        // }
+        
+        // if (totalSeconds < 60) {
+        //     System.out.println("Seconds: " + totalSeconds);
+        // }
     }
 }
--- src/SecAndMin.java
+++ src/SecAndMin.java
@@ -1,30 +1,40 @@
 /* Challenge tips:
--> Use int & long for your number data types
+-> Use int or long for your number data types
 -> 1 minute = 60 seconds
 -> 1 hour = 60 minutes (or 3600 seconds)
 */
 
 public class SecAndMin {
-    public String getDurationString(int minutes, int seconds) {
-        // Validate
-        if ((minutes >= 0) || ((seconds >= 0) && (seconds <= 59))) {
-            // Calculate how many hours, minutes, & seconds equal the minutes & seconds passed to this method
+    // public String getDurationString(int minutes, int seconds) {
+    //     int totalSeconds;
+    //     int hours;
+    //     int min;
+    //     int sec;
 
-            // Return that value as a String, formatted like so: XXh YYm ZZs (num hours, num minutes, & num seconds)
-        } else {
-            return "Invalid value";
-        }
-    }
+    //     // Validate
+    //     if ((minutes >= 0) || ((seconds >= 0) && (seconds <= 59))) {
+    //         totalSeconds = (minutes * 60) + seconds;
+
+    //         if (totalSeconds >= 3600) {
+    //             min = totalSeconds / 60;
+    //         }
+
+    //         // Return as a String, formatted like so: XXh YYm ZZs (num hours, num minutes, & num seconds)
+    //         return hours + "h " + min + "m " + sec + "s";
+    //     } else {
+    //         return "Invalid value";
+    //     }
+    // }
 
     // Method overloading
-    public String getDurationString(int seconds) {
-        // Validate
-        if (seconds >= 0) {
-            // Calculate how many minutes are in the seconds value
+    // public String getDurationString(int seconds) {
+    //     // Validate
+    //     if (seconds >= 0) {
+    //         // Calculate how many minutes are in the seconds value
 
-            // Call the other overloaded method, passing the correct minutes & seconds calculated so that it can work properly
-        } else {
-            return "Invalid value";
-        }
-    }
+    //         // Call the other overloaded method, passing the correct minutes & seconds calculated so that it can work properly
+    //     } else {
+    //         return "Invalid value";
+    //     }
+    // }
 }
