ccabc502ee38c9b3bd97ac057521e98411dcf0a8
dayna-schlenk <dayna.schlenker@gmail.com>
2022-04-03 20:20:10 -0500

    Test instructor's first method against mine

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 public class App {
     public static void main(String[] args) {
         SecAndMin myTry = new SecAndMin();
-        System.out.println(myTry.getDurationString(6200));
+        System.out.println(myTry.getDurationString(65, 45));
+
+        SecAndMin2 myTry2 = new SecAndMin2();
+        System.out.println(myTry2.getDurationString(65, 45));
     }
 }
