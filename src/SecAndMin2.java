a24e20b67db463a5ae7d208bed28a36869056d18
dayna-schlenk <dayna.schlenker@gmail.com>
2022-04-03 20:17:16 -0500

    Create first method in instructor solution

--- src/SecAndMin2.java
+++ src/SecAndMin2.java
@@ -7,12 +7,18 @@
 // INSTRUCTOR SOLUTION
 
 public class SecAndMin2 {
-    public String getDurationString(int minutes, int seconds) {
-        // code
+    public String getDurationString(long minutes, long seconds) { // int is okay as well
+        if ((minutes < 0) || (seconds < 0) || (seconds > 59)) {
+            return "Invalid value";
+        }
+
+        long hours = minutes / 60;
+        long remainingMinutes = minutes % 60;
+        return hours + "h " + remainingMinutes + "m " + seconds + "s";
     }
 
     // Method overloading
-    public String getDurationString(int seconds) {
-        // code
-    }
+    // public String getDurationString(long seconds) { // int is okay as well
+    //     // code
+    // }
 }
