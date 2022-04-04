f7ed310ce5c2885cc0f3e636009deea55d7768be
dayna-schlenk <dayna.schlenker@gmail.com>
2022-04-03 20:06:48 -0500

    Set up SecAndMin2 class for instructor solution

---
+++ src/SecAndMin2.java
@@ -0,0 +1,18 @@
+/* Challenge tips:
+-> Use int or long for your number data types
+-> 1 minute = 60 seconds
+-> 1 hour = 60 minutes (or 3600 seconds)
+*/
+
+// INSTRUCTOR SOLUTION
+
+public class SecAndMin2 {
+    public String getDurationString(int minutes, int seconds) {
+        // code
+    }
+
+    // Method overloading
+    public String getDurationString(int seconds) {
+        // code
+    }
+}
