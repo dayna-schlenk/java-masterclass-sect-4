9b1b48cc3dc8e3cf15dfa0b23a2f70d4cec8e728
dayna-schlenk <dayna.schlenker@gmail.com>
2022-04-07 16:26:19 -0500

    Create MinToYearsAndDays class

--- src/App.java
+++ src/App.java
@@ -1,7 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        AreaCalculator myTry = new AreaCalculator();
-        System.out.println(myTry.area(5.0));
-        System.out.println(myTry.area(-1.0, 4.0));
+        // code
     }
 }
