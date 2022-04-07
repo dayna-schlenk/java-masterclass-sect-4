e5d90d699be01af711e8da10542a1d5f1fb87bb5
dayna-schlenk <dayna.schlenker@gmail.com>
2022-04-07 16:21:19 -0500

    Create overloaded method for rectangle area

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -7,4 +7,14 @@ public class AreaCalculator {
         double circleArea = Math.PI * Math.pow(radius, 2);
         return circleArea;
     }
+
+    // Overloaded method
+    public double area(double x, double y) {
+        if (x < 0 || y < 0) {
+            return -1.0;
+        }
+
+        double rectangleArea = x * y;
+        return rectangleArea;
+    }
 }
