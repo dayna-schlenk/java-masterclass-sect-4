4f05e1588da4c907a66c9f4655bb8975606b08b3
dayna-schlenk <dayna.schlenker@gmail.com>
2022-04-07 16:16:00 -0500

    Create first method for area

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -1,3 +1,10 @@
 public class AreaCalculator {
-    // code
+    public double area(double radius) {
+        if (radius < 0) {
+            return -1.0;
+        }
+        
+        double circleArea = Math.PI * Math.pow(radius, 2);
+        return circleArea;
+    }
 }
