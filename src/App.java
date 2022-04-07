89f48f5146e5c8510798217229fc73cca9aa46dd
dayna-schlenk <dayna.schlenker@gmail.com>
2022-04-07 16:25:21 -0500

    Test both methods and solve challenge

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        // code
+        AreaCalculator myTry = new AreaCalculator();
+        System.out.println(myTry.area(5.0));
+        System.out.println(myTry.area(-1.0, 4.0));
     }
 }
