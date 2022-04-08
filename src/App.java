af2c4e998290c93900a088b21e20a674b4d6e8ef
dayna-schlenk <dayna.schlenker@gmail.com>
2022-04-08 11:20:13 -0500

    Create EqualityPrinter class

--- src/App.java
+++ src/App.java
@@ -1,7 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        MinToYearsAndDays myTry = new MinToYearsAndDays();
-
-        myTry.printYearsAndDays(-1);
+        // code
     }
 }
