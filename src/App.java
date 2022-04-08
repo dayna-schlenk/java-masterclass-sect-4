cc4a5d4cdb17ffa86c78c79ccdbed05a265ba197
dayna-schlenk <dayna.schlenker@gmail.com>
2022-04-08 10:55:04 -0500

    Solve Min to Years and Days challenge

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        // code
+        MinToYearsAndDays myTry = new MinToYearsAndDays();
+
+        myTry.printYearsAndDays(-1);
     }
 }
--- src/MinToYearsAndDays.java
+++ src/MinToYearsAndDays.java
@@ -1,3 +1,18 @@
 public class MinToYearsAndDays {
-    // code
+    public void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            // Minutes to hours
+            long hours = minutes / 60;
+            // Hours to days
+            long days = hours / 24;
+            // Days to years
+            long years = days / 365;
+            // Get remaining days, if any
+            days %= 365;
+
+            System.out.println(minutes + " min = " + years + " y and " + days + " d");
+        }
+    }
 }
