c9beb55b5c7fcca9ed60c1fac7c872a3f52da463
dayna-schlenk <dayna.schlenker@gmail.com>
2022-04-09 10:49:54 -0500

    Refactor printEqual method

--- src/EqualityPrinter.java
+++ src/EqualityPrinter.java
@@ -1,18 +1,14 @@
 public class EqualityPrinter {
     public void printEqual(int x, int y, int z) {
-        String xNum = String.valueOf(x);
-        String yNum = String.valueOf(y);
-        String zNum = String.valueOf(z);
-
         if (x < 0 || y < 0 || z < 0) {
             System.out.println("Invalid Value");
         } else {
-            if ((xNum.compareTo(yNum) == 0) && (xNum.compareTo(zNum) == 0)) {
+            if ((x == y) && (y == z)) {
                 System.out.println("All numbers are equal");          
-            } else if ((xNum.compareTo(yNum) == 0) || (xNum.compareTo(zNum) == 0) || (zNum.compareTo(yNum) == 0)) {
-                System.out.println("Neither all are equal or different");
-            } else {
+            } else if ((x != y) && (x != z) && (y != z)) {
                 System.out.println("All numbers are different");
+            } else {
+                System.out.println("Neither all are equal or different");
             }
         }
     }
